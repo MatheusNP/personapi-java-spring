25f4932ec5bebc212bf9e3e3e42bcd8469710f7e
Matheus Nobre <matheusnobre56@gmail.com>
2021-07-25 21:07:06 -0300

    First classes using Lombok API

---
+++ src/main/java/one/digitalinnovation/personapi/controller/PersonController.java
@@ -0,0 +1,17 @@
+package one.digitalinnovation.personapi.controller;
+
+import one.digitalinnovation.personapi.entity.Person;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/person")
+public class PersonController {
+
+    @GetMapping
+    public String getPerson() {
+
+        return "API Test";
+    }
+}
